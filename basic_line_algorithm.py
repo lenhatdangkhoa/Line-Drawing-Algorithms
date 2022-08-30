54f4a204b373b28f6498b41d5b0d761a615d027c
Khoa Le <kl58277@uga.edu>
2022-08-30 17:36:33 -0400

    Testing multiple lines

--- basic_line_algorithm.py
+++ basic_line_algorithm.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
 image = Image.new(mode="RGB", size = (500, 500), color = (0,0,0))
-for i in range(50):
-    image.putpixel((i,i), (255,255,255))
+for i in range(200):
+    image.putpixel((250,i), (255,255,255))
+    image.putpixel((250 + i, 200), (255,255,255))
 image.show()
