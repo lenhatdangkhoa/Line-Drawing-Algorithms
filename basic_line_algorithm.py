cd04b0637d075ec08f2ba554159ab3fb02bbac49
Khoa Le <kl58277@uga.edu>
2022-08-30 16:14:57 -0400

    Creating a blank black screen for pixel display

--- basic_line_algorithm.py
+++ basic_line_algorithm.py
@@ -1,3 +1,6 @@
 from PIL import Image
 
-print("Hello World!")
+image = Image.new(mode="RGB", size = (500, 500), color = (0,0,0))
+for i in range(50):
+    image.putpixel((i,i), (255,255,255))
+image.show()
