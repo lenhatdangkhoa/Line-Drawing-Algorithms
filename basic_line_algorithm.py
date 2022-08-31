935e647362905bafafd0239d82303be0788c32aa
Khoa Le <kl58277@uga.edu>
2022-08-31 14:40:38 -0400

    Trying to write the coordinates to a new file

--- basic_line_algorithm.py
+++ basic_line_algorithm.py
@@ -1,7 +1,14 @@
-from PIL import Image
+from PIL import Image # Python Imaging Library
 
+# Creating a blank dark screen
 image = Image.new(mode="RGB", size = (500, 500), color = (0,0,0))
+
 for i in range(200):
     image.putpixel((250,i), (255,255,255))
     image.putpixel((250 + i, 200), (255,255,255))
+
+
+with open("coordinates.txt", 'w') as filename:
+    for i in range(10):
+        filename.write(f"{i}\n")
 image.show()
