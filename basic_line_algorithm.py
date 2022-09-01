c9b393ffe4ba9f854fd370fa9be5472c0f7b5dcd
Khoa Le <kl58277@uga.edu>
2022-09-01 16:43:34 -0400

    Testing slope

--- basic_line_algorithm.py
+++ basic_line_algorithm.py
@@ -11,4 +11,19 @@ for i in range(200):
 with open("coordinates.txt", 'w') as filename:
     for i in range(10):
         filename.write(f"{i}\n")
-image.show()
+#image.show()
+
+"""
+The following function will draw a line at the two coordinates:
+(x0,y0) and (x1, y1)
+"""
+def draw_basic_line(x0, y0, x1, y1):
+    try:
+        slope = (y1 - y0) / (x1 - x0)
+        print(slope)
+    except ZeroDivisionError:
+        print("Cannot divide by zero!")
+   
+
+draw_basic_line( 10, 10 , 10 , 10)
+
